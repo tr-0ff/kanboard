572adcc73ee4db6475ade9e1acfc9988f5718b5c
Hady Mahmoud <hady.mahmoud@programmfabrik.de>
2021-03-16 16:00:19 +0100

    Add custom database and email config from ENV

--- docker/var/www/app/config.php
+++ docker/var/www/app/config.php
@@ -2,3 +2,14 @@
 
 defined('ENABLE_URL_REWRITE') or define('ENABLE_URL_REWRITE', true);
 defined('LOG_DRIVER') or define('LOG_DRIVER', 'system');
+
+# Database configurations
+define('DB_DRIVER', 'postgres');
+define('DB_USERNAME', 'kanboard_dbuser');
+define('DB_PASSWORD', $_ENV['KANBOARD_DBPASS']);
+define('DB_HOSTNAME', $_ENV['KANBOARD_DBHOST']);
+define('DB_NAME', 'kanboard');
+
+# Email configurations
+define('MAIL_FROM', $_ENV['KANBOARD_EMAIL_FROM']);
+
